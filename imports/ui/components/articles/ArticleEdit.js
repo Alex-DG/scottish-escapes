eb41713fbf9c05faba9797a80ce063e87884d730
Alexandre Di Guida <alex@gearedapp.co.uk>
2017-06-08 22:47:10 +0100

    Add more styles - Add Drop zone for Thumbnail

--- imports/ui/components/articles/ArticleEdit.js
+++ imports/ui/components/articles/ArticleEdit.js
@@ -10,12 +10,13 @@ const propTypes = {
 };
 
 const defaultProps = {
- article: {
-   title: '',
-   location: '',
-   description: '',
-   thumbnail: '',
-   images: [],
+  preview: undefined,
+  article: {
+    title: '',
+    location: '',
+    description: '',
+    thumbnail: '',
+    images: [],
  },
 };
 
@@ -29,8 +30,6 @@ class ArticleEdit extends Component {
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSaveArticle = this.handleSaveArticle.bind(this);
-
-    this.handleThumbnail = this.handleThumbnail.bind(this);
     this.handleDropImages = this.handleDropImages.bind(this);
   }
 
@@ -55,18 +54,16 @@ class ArticleEdit extends Component {
     this.setState({ article: articleUpdated });
   }
 
-  handleThumbnail() {
-    const file = this.fileUpload.files[0];
-
+  handleDropImage(files) {
+    console.log(files);
+    const file = files[0];
     if (file) {
-      let reader = new FileReader();
-      let url = reader.readAsDataURL(file);
-
-      reader.onloadend = function (e) {
-              this.setState({
-                  preview: reader.result,
-              })
-            }.bind(this);
+      this.setState({ preview: file.preview });
+      // let reader = new FileReader();
+      // let url = reader.readAsDataURL(file);
+      // reader.onloadend = function (e) {
+      //   this.setState({ preview: reader.result });
+      // }.bind(this);
     }
   }
 
@@ -75,6 +72,7 @@ class ArticleEdit extends Component {
   }
 
   render() {
+    console.log(this.state.preview);
     return(
       <div>
         <div className="align">
@@ -86,64 +84,70 @@ class ArticleEdit extends Component {
         </div>
 
         <div className="margin-top-15">
-          <div className="container">
-            <form className="form-horizontal">
+          <div className="panel panel-default">
+            <div className="panel-body">
+              <form className="form-horizontal">
 
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="title"
-                  placeholder="Enter title"
-                  onChange={ this.handleInputChange }
-                  defaultValue={ this.state.article.title } />
-              </div>
-
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="location"
-                  placeholder="Enter location"
-                  onChange={ this.handleInputChange }
-                  defaultValue={ this.state.article.location } />
-              </div>
-
-              <div className="form-group">
-                <div className="resize-container">
-                  <DraftEditor
-                    name="description"
-                    placeholder="Enter description"
-                    onChange={ this.handleInputChange } />
-                </div>
-              </div>
-
-              {/* <div className="form-group row">
-                <div className="col-xs-12 col-sm-3">
-                  <label htmlFor="thumbnailfile">Select Thumbnail</label>
+                <div className="form-group">
                   <input
-                    type='file' accept='.jpg'
-                    name="thumbnail"
-                    onChange={ this.handleThumbnail }
-                    ref={ (ref) => this.fileUpload = ref } />
-                  <small id="fileHelp" className="form-text text-muted">Format: .jpg</small>
+                    type="text"
+                    className="form-control"
+                    name="title"
+                    placeholder="Enter title"
+                    onChange={ this.handleInputChange }
+                    defaultValue={ this.state.article.title } />
                 </div>
 
-                <div className="col-xs-12 col-sm-4">
-                  <Thumbnail index={ 0 } url={ this.state.preview } />
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="location"
+                    placeholder="Enter location"
+                    onChange={ this.handleInputChange }
+                    defaultValue={ this.state.article.location } />
                 </div>
-              </div>
 
-              <div className="form-group row">
-                <Dropzone onDrop={ this.handleDropImages }>
-                  <div className="full-height flex-center-vertical-container">
-                    <span>[ Drop your images here, or click to select one ]</span>
+                <div className="form-group">
+                  <div className="resize-container">
+                    <DraftEditor
+                      name="description"
+                      placeholder="Enter description"
+                      onChange={ this.handleInputChange } />
                   </div>
-                </Dropzone>
-              </div> */}
+                </div>
 
-              {/* <button type="submit" className="btn btn-primary">Submit</button> */}
-            </form>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div className="margin-top-15">
+          <div className="panel panel-default">
+            <div className="panel-body">
+              <form className="form-horizontal">
+
+                <div className="form-group">
+                  <div className="col-xs-6 col-md-6">
+                    <Dropzone
+                      className="dropzone-container"
+                      onDrop={ this.handleDropImage }>
+                      <div className="align">
+                        <span>[ Thumbnail: Drop or Select your file ]</span>
+                      </div>
+                    </Dropzone>
+                  </div>
+                  <div className="col-xs-6 col-md-6">
+                    <div className="thumbnail">
+                      <div>
+                         <img src={ this.state.preview } />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </form>
+            </div>
           </div>
         </div>
       </div>
